9aff72affe7fe50ef992ba39b33eb725d1cc83c7
mareting-shibata50 <marketing.shibata50@gmail.com>
2021-05-31 16:36:41 +0900

    5rd Select Root

--- liny/liny.py
+++ liny/liny.py
@@ -9,15 +9,10 @@ from google.oauth2.service_account import Credentials
 
 st.title('Liny数字管理')
 
-st.write("""
-# 設定
-以下のオプションから表示する範囲を指定
-""")
-
 def get_spred():
     # スプレッドシートの読み込み
     scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-    credentials = Credentials.from_service_account_file('liny/service_account.json', scopes=scopes)
+    credentials = Credentials.from_service_account_file('service_account.json', scopes=scopes)
     gc = gspread.authorize(credentials)
     SP_SHEET_KEY_MEMBER = '13QemJFcysc703EpF7PsnJWDQyO6KiXFPWiGBkQ-cw6U'
     sh_member = gc.open_by_key(SP_SHEET_KEY_MEMBER)
@@ -36,19 +31,18 @@ def get_data():
     db = db[['登録(フォロー)日時', 'ユーザーブロック', '流入時シナリオ', '希望勤務地', '個人情報入力経路', '流入経路', '流入経路詳細', '卒業年度', '電話番号']]
     db = db.rename(columns={'登録(フォロー)日時': 'Date', 'ユーザーブロック': '友だち', '電話番号': '個人情報'})
     db['Month'] = db['Date'].dt.strftime('%Y-%m')
-    db['Date'] = db['Date'].dt.strftime('%Y%m%d')
     db['Date'] = db['Date'].astype('datetime64')
     db = db.replace(r'^\s*$', np.nan, regex=True)
     return db
 
-def get_chart(data):
+def get_chart(data, period):
     data = data.reset_index()
     ymin1 = 0
     ymax1 = data['友だち'].max()
     ymin2 = 0
     ymax2 = 1
     base = alt.Chart(data).encode(
-        alt.X('Date:T', axis=alt.Axis(title=None))
+        alt.X(period+':T', axis=alt.Axis(title=None))
     )
     line1 = base.mark_line(color='#ff0000').encode(
         alt.Y('友だち',
@@ -72,11 +66,17 @@ def get_chart(data):
     )
     return chart
 
-# try:
+def calc_db(temp_db, period):
+    data = pd.pivot_table(data = temp_db, values = ["友だち", "卒業年度", "個人情報"], aggfunc ="count", index = period, margins=False, fill_value=0)
+    data['卒業年度%'] = (data['卒業年度'] / data['友だち'])
+    data['個人情報%'] = (data['個人情報'] / data['卒業年度'])
+    data['全体%'] = (data['個人情報'] / data['友だち'])
+    # data.index = data.index.strftime('%Y/%m/%d')
+    st.write(data.T)
+    return get_chart(data, period)
 
-st.write(""" ## 表示日数選択 """)
+st.write(""" # 表示日数選択 """)
 db = get_data()
-# db = db.loc[]
 
 mindate = dt.date(2020,8,1)
 maxdate = db['Date'].max()
@@ -91,41 +91,35 @@ s_time, e_time = st.date_input(
 s_time = np.datetime64(s_time)
 e_time = np.datetime64(e_time)
 temp_db = db[(db['Date'] >= s_time) & (db['Date'] <= e_time)]
+temp_db['Date'] = temp_db['Date'].dt.strftime('%Y/%m/%d')
 
-data = pd.pivot_table(data = temp_db, values = ["友だち", "卒業年度", "個人情報"], aggfunc ="count", index = "Date", margins=False, fill_value=0)
-# data['卒業年度%'] = (data['卒業年度'] / data['友だち']).map('{:.2%}'.format)
-# data['個人情報%'] = (data['個人情報'] / data['卒業年度']).map('{:.2%}'.format)
-data['卒業年度%'] = (data['卒業年度'] / data['友だち'])
-data['個人情報%'] = (data['個人情報'] / data['卒業年度'])
-data['全体%'] = (data['個人情報'] / data['友だち'])
-data.index = data.index.strftime('%Y/%m/%d')
-data = data.T
+st.write("# 全体")
+# st.write("##  日別の数字")
+# chart = calc_db(temp_db, 'Date')
+# st.altair_chart(chart, use_container_width=True)
 
-st.write(f"""
-###  日別の数字
-""")
-st.write(data)
-temp_data = data.T
-# temp_data = temp_data.T.reset_index()
-# temp_data = pd.melt(temp_data, id_vars=['Date'])
-chart = get_chart(temp_data)
+st.write("##  月別の数字")
+chart = calc_db(temp_db, 'Month')
 st.altair_chart(chart, use_container_width=True)
 
-st.write(f"""
-###  月別の数字
-""")
-mdata = pd.pivot_table(data = temp_db, values = ["友だち", "卒業年度", "個人情報"], aggfunc ="count", index = "Month", margins=False, fill_value=0)
-# data['卒業年度%'] = (data['卒業年度'] / data['友だち']).map('{:.2%}'.format)
-# data['個人情報%'] = (data['個人情報'] / data['卒業年度']).map('{:.2%}'.format)
-mdata['卒業年度%'] = (mdata['卒業年度'] / mdata['友だち'])
-mdata['個人情報%'] = (mdata['個人情報'] / mdata['卒業年度'])
-mdata['全体%'] = (mdata['個人情報'] / mdata['友だち'])
-mdata = mdata.T
-st.write(mdata)
-temp_mdata = mdata
-temp_mdata = temp_mdata.T.reset_index()
-temp_mdata = temp_mdata.rename(columns={'Month': 'Date'})
-chart = get_chart(temp_mdata)
+st.write("# 就活市場経由")
+temp_db_s = temp_db[temp_db.流入経路.eq('就活市場')]
+
+# st.write("##  日別の数字")
+# chart = calc_db(temp_db_s, 'Date')
+# st.altair_chart(chart, use_container_width=True)
+
+st.write("##  月別の数字")
+chart = calc_db(temp_db_s, 'Month')
 st.altair_chart(chart, use_container_width=True)
-# except:
-#     st.error("おっと！何かエラーが起きているようです。")
+
+st.write("# 広告経由")
+temp_db_a = temp_db[temp_db.流入経路.eq('LP経由')]
+
+# st.write("##  日別の数字")
+# chart = calc_db(temp_db_a, 'Date')
+# st.altair_chart(chart, use_container_width=True)
+
+st.write("##  月別の数字")
+chart = calc_db(temp_db_a, 'Month')
+st.altair_chart(chart, use_container_width=True)

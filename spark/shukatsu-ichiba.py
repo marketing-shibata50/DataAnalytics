e25c397968c4fda3bbebbd236169c9e04a1cf6b4
marketing.shibata50 <marketing.shibata50@gmail.com>
2021-06-01 01:21:14 +0900

    7th Commit Add Daily & Monthly

--- spark/shukatsu-ichiba.py
+++ spark/shukatsu-ichiba.py
@@ -1,5 +1,7 @@
 import re
+from altair.vegalite.v4.api import concat
 from altair.vegalite.v4.schema.channels import Column
+from pandas.core import groupby
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -10,6 +12,7 @@ from bs4 import BeautifulSoup
 import requests
 import datetime as dt
 from gspread_dataframe import set_with_dataframe
+import re
 
 def read_spred_keys():
     # スプレッドシートの読み込み
@@ -17,37 +20,26 @@ def read_spred_keys():
     credentials = Credentials.from_service_account_file('service_account.json', scopes=scopes)
     gc = gspread.authorize(credentials)
 
-    SP_SHEET_KEY_NUMBER = '1DBhckcpEKz2mJliq6UAO7BJrHlScvL1oY5QbwObih0U'
+    SP_SHEET_KEY_SPARK = '1Tg7M8InNGxVmtQD01ujK6tFWTrmEAyU1pxqo9XCFtIg'
     SP_SHEET_KEY_MEMBER = '13QemJFcysc703EpF7PsnJWDQyO6KiXFPWiGBkQ-cw6U'
+    SP_SHEET_KEY_MASTER = '1oeORQTjudO9-B4JjyFqilHtrOQtlvOmdUNEp8NnwSzI'
 
-    sh_number = gc.open_by_key(SP_SHEET_KEY_NUMBER)
+    sh_spark = gc.open_by_key(SP_SHEET_KEY_SPARK)
     sh_member = gc.open_by_key(SP_SHEET_KEY_MEMBER)
+    sh_master = gc.open_by_key(SP_SHEET_KEY_MASTER)
 
-    return sh_number, sh_member
+    return sh_spark, sh_member, sh_master
 
 def read_spred_tab():
     SP_SHEET = {
-        'SP_SHEET_WHOLE': '全体',
-        'SP_SHEET_LP': 'LP',
-        'SP_SHEET_EBOOK': 'ebook',
-        'SP_SHEET_AGENT': 'agent',
+        'SP_SHEET_IMP': '【計算用】表示回数',
+        'SP_SHEET_CT': '【更新】クリック'
     }
     SP_SHEET_REGISTER = {
         'SP_SHEET_R': '貼付：会員登録',
         'SP_SHEET_L': '貼付：Liny'
     }
     return SP_SHEET, SP_SHEET_REGISTER
-
-@st.cache
-def read_spred_number(SP_SHEET_XXX):
-    data = pd.DataFrame()
-    worksheet = sh_number.worksheet(SP_SHEET[SP_SHEET_XXX])
-    temp_data = worksheet.get_all_values()
-    data = pd.DataFrame(temp_data[15:], columns=temp_data[14])
-    data[['Pageviews', 'Unique Pageviews']] = data[['Pageviews', 'Unique Pageviews']].astype(int)
-    data['Date'] = pd.to_datetime(data['Date'])
-    data['Month'] = data['Date'].dt.strftime("%Y-%m")
-    return data
 
 def get_chart(values_month):
     values_month = values_month.reset_index()
@@ -76,40 +68,94 @@ def get_chart(values_month):
     )
     return chart
 
-sh_number, sh_member = read_spred_keys()
-SP_SHEET, SP_SHEET_REGISTER = read_spred_tab()
-whole_data = read_spred_number('SP_SHEET_WHOLE')
-lp_data = read_spred_number('SP_SHEET_LP')
-ebook_data = read_spred_number('SP_SHEET_EBOOK')
-agent_data = read_spred_number('SP_SHEET_AGENT')
+def make_register(db):
+    db = db[['Date', 'ID']]
 
+    db['pattern'] = db['ID'].str[-1]
+    temp = db.loc[db['pattern']=='p']
+    temp['ID'] = temp['ID'].str[:-1]
+    db['ID'].loc[db['pattern']=='p'] = temp['ID']
+    db['CV'] = 1
+    db['Date'] = db['Date'].str.split(' ', expand=True)[0]
+
+    db = pd.pivot_table(data = db, values = "CV", aggfunc ="count", index = ["Date", "ID"], margins=False, fill_value=0)
+    db = db.reset_index()
+    db['CV'] = db['CV'].astype(int)
+    return db
+
+def make_datetime(db):
+    db['Date'] = pd.to_datetime(db['Date'])
+    db['Date'] = db['Date'].dt.strftime('%Y/%m/%d')
+    return db
+
+def calc_per(db):
+    db['CTR'] = db['CT'] / db['Imp']
+    db['CVR'] = db['CV'] / db['CT']
+    db = pd.merge(db, master, on='ID', how='left')
+    db = db.fillna(0)
+    st.write(db)
+    return db
+
+# スプレッドの情報抜き出し
+sh_spark, sh_member, sh_master = read_spred_keys()
+SP_SHEET, SP_SHEET_REGISTER = read_spred_tab()
+
+# マスターデータ
+worksheet = sh_master.worksheet('マスターデータ')
+temp_master = worksheet.get_all_values()
+master = pd.DataFrame(temp_master[4:], columns=temp_master[3])
+master = master[['ID', '進捗', '担当', 'ニーズ']]
+master = master.replace(r'^\s*$', 0, regex=True)
+master = master.loc[master['ID']!=0]
+st.write(master)
+
+# 表示回数の計算
+imp = pd.DataFrame()
+worksheet = sh_spark.worksheet(SP_SHEET['SP_SHEET_IMP'])
+temp_imp = worksheet.get_all_values()
+imp = pd.DataFrame(temp_imp[6:], columns=temp_imp[4]).rename(columns={'ID': 'Date'})
+imp = pd.melt(imp, id_vars=['Date']).rename(columns={'value': 'Imp', 'variable': 'ID'})
+imp = imp.replace(r'^\s*$', 0, regex=True)
+imp['Imp'] = imp['Imp'].astype(int)
+imp = make_datetime(imp)
+
+# CT数の計算
+ct = pd.DataFrame()
+worksheet = sh_spark.worksheet(SP_SHEET['SP_SHEET_CT'])
+temp_ct = worksheet.get_all_values()
+ct = pd.DataFrame(temp_ct[15:], columns=temp_ct[14]).rename(columns={'Total Events': 'CT', 'Event Label': 'ID'})
+ct['pattern'] = ct['ID'].str[-1]
+temp = ct.loc[ct['pattern']=='p']
+temp['ID'] = temp['ID'].str[:-1]
+ct['ID'].loc[ct['pattern']=='p'] = temp['ID']
+ct = ct.groupby(['Date', 'ID']).sum()['CT']
+ct = ct.reset_index()
+ct['CT'] = ct['CT'].astype(int)
+ct = make_datetime(ct)
+
+# 会員登録の計算
 worksheet = sh_member.worksheet(SP_SHEET_REGISTER['SP_SHEET_R'])
 temp_data = worksheet.get_all_values()
-register_R = pd.DataFrame(temp_data[1:], columns=temp_data[0])
-register_R['登録日時'] = register_R['登録日時'].str.split(' ', expand=True)[0]
-register_R['登録日時'] = pd.to_datetime(register_R['登録日時'])
-register_R['Month'] = register_R['登録日時'].dt.strftime("%Y-%m")
+register_R = pd.DataFrame(temp_data[1:], columns=temp_data[0]).rename(columns={'ID': '学生ID', '登録日時': 'Date', '経由点(バナー)': 'ID'})
+val_register_R = make_register(register_R)
 
+# LINE登録の計算
 worksheet = sh_member.worksheet(SP_SHEET_REGISTER['SP_SHEET_L'])
 temp_data = worksheet.get_all_values()
-register_L = pd.DataFrame(temp_data[2:], columns=temp_data[1])
-register_L['登録(フォロー)日時'] = register_L['登録(フォロー)日時'].str.split(' ', expand=True)[0]
-register_L['登録(フォロー)日時'] = pd.to_datetime(register_L['登録(フォロー)日時'])
-register_L['Month'] = register_L['登録(フォロー)日時'].dt.strftime("%Y-%m")
+register_L = pd.DataFrame(temp_data[2:], columns=temp_data[1]).rename(columns={'ID': '学生ID', '登録(フォロー)日時': 'Date', '流入経路詳細': 'ID'})
+val_register_L = make_register(register_L)
 
-values_month = pd.DataFrame(whole_data.groupby('Month').sum()['Unique Pageviews']).rename(columns={'Unique Pageviews': 'UU'})
-temp_values_l = pd.DataFrame(lp_data.groupby('Month').sum()['Unique Pageviews']).rename(columns={'Unique Pageviews': 'LP'})
-temp_values_e = pd.DataFrame(ebook_data.groupby('Month').sum()['Unique Pageviews']).rename(columns={'Unique Pageviews': 'ebook'})
-temp_values_a = pd.DataFrame(agent_data.groupby('Month').sum()['Unique Pageviews']).rename(columns={'Unique Pageviews': 'agent'})
-temp_values_R = pd.DataFrame(register_R.groupby('Month').count()['ID']).rename(columns={'ID': '登録'})
-temp_values_L = pd.DataFrame(register_L.groupby('Month').count()['ID']).rename(columns={'ID': 'LINE'})
-values_month = pd.merge(values_month, temp_values_l, left_index=True, right_index=True, how="left")
-values_month = pd.merge(values_month, temp_values_e, left_index=True, right_index=True, how="left")
-values_month = pd.merge(values_month, temp_values_a, left_index=True, right_index=True, how="left")
-values_month = pd.merge(values_month, temp_values_R, left_index=True, right_index=True, how="left")
-values_month = pd.merge(values_month, temp_values_L, left_index=True, right_index=True, how="left")
-values_month = values_month.fillna(0)
+val_register = pd.concat([val_register_R, val_register_L])
+val_register = make_datetime(val_register)
 
-st.write(values_month.T)
-chart = get_chart(values_month)
-st.altair_chart(chart, use_container_width=True)
+val_daily = pd.merge(imp, ct, on=['Date', 'ID'], how='left')
+val_daily = pd.merge(val_daily, val_register, on=['Date', 'ID'], how='left')
+val_daily = calc_per(val_daily)
+
+val_month = val_daily
+val_month['Date'] = pd.to_datetime(val_month['Date'])
+val_month['Month'] = val_month['Date'].dt.strftime('%Y%m')
+del val_month['Date']
+val_month = val_month.groupby(['Month', 'ID']).sum()[['Imp', 'CT', 'CV']]
+val_month = val_month.reset_index()
+val_month = calc_per(val_month)

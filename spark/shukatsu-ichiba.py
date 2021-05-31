60d16231526ce7064a08f1117cab78259e192349
marketing.shibata50 <marketing.shibata50@gmail.com>
2021-06-01 01:25:30 +0900

    7th Commit Add Daily & Monthly

--- spark/shukatsu-ichiba.py
+++ spark/shukatsu-ichiba.py
@@ -68,7 +68,6 @@ def get_chart(values_month):
     )
     return chart
 
-<<<<<<< HEAD
 def make_register(db):
     db = db[['Date', 'ID']]
 
@@ -97,56 +96,33 @@ def calc_per(db):
     st.write(db)
     return db
 
-=======
->>>>>>> ca8b102ccae85e1bc320bd0950a852ada3323d66
 # スプレッドの情報抜き出し
 sh_spark, sh_member, sh_master = read_spred_keys()
 SP_SHEET, SP_SHEET_REGISTER = read_spred_tab()
 
-<<<<<<< HEAD
-=======
-# 初期設定
-val = pd.DataFrame()
-
->>>>>>> ca8b102ccae85e1bc320bd0950a852ada3323d66
 # マスターデータ
 worksheet = sh_master.worksheet('マスターデータ')
 temp_master = worksheet.get_all_values()
 master = pd.DataFrame(temp_master[4:], columns=temp_master[3])
 master = master[['ID', '進捗', '担当', 'ニーズ']]
-<<<<<<< HEAD
 master = master.replace(r'^\s*$', 0, regex=True)
 master = master.loc[master['ID']!=0]
-=======
->>>>>>> ca8b102ccae85e1bc320bd0950a852ada3323d66
 st.write(master)
 
 # 表示回数の計算
 imp = pd.DataFrame()
 worksheet = sh_spark.worksheet(SP_SHEET['SP_SHEET_IMP'])
 temp_imp = worksheet.get_all_values()
-<<<<<<< HEAD
 imp = pd.DataFrame(temp_imp[6:], columns=temp_imp[4]).rename(columns={'ID': 'Date'})
 imp = pd.melt(imp, id_vars=['Date']).rename(columns={'value': 'Imp', 'variable': 'ID'})
 imp = imp.replace(r'^\s*$', 0, regex=True)
 imp['Imp'] = imp['Imp'].astype(int)
 imp = make_datetime(imp)
-=======
-imp = pd.DataFrame(temp_imp[6:], columns=temp_imp[4])
-imp = imp.rename(columns={'ID': 'Date'})
-imp['Date'] = pd.to_datetime(imp['Date'])
-imp['Date'] = imp['Date'].dt.strftime('%Y/%m/%d')
-imp = imp.replace(r'^\s*$', 0, regex=True)
-imp = pd.melt(imp, id_vars=['Date']).rename(columns={'value': 'Count', 'variable': 'ID'})
-imp['type'] = 'imp'
-val = imp
->>>>>>> ca8b102ccae85e1bc320bd0950a852ada3323d66
 
 # CT数の計算
 ct = pd.DataFrame()
 worksheet = sh_spark.worksheet(SP_SHEET['SP_SHEET_CT'])
 temp_ct = worksheet.get_all_values()
-<<<<<<< HEAD
 ct = pd.DataFrame(temp_ct[15:], columns=temp_ct[14]).rename(columns={'Total Events': 'CT', 'Event Label': 'ID'})
 ct['pattern'] = ct['ID'].str[-1]
 temp = ct.loc[ct['pattern']=='p']
@@ -156,41 +132,17 @@ ct = ct.groupby(['Date', 'ID']).sum()['CT']
 ct = ct.reset_index()
 ct['CT'] = ct['CT'].astype(int)
 ct = make_datetime(ct)
-=======
-ct = pd.DataFrame(temp_ct[15:], columns=temp_ct[14]).rename(columns={'Total Events': 'Count', 'Event Label': 'ID'})
-ct['Date'] = pd.to_datetime(ct['Date'])
-ct['Date'] = ct['Date'].dt.strftime('%Y/%m/%d')
-ct['type'] = 'ct'
-val = pd.concat([val,ct])
->>>>>>> ca8b102ccae85e1bc320bd0950a852ada3323d66
 
 # 会員登録の計算
 worksheet = sh_member.worksheet(SP_SHEET_REGISTER['SP_SHEET_R'])
 temp_data = worksheet.get_all_values()
 register_R = pd.DataFrame(temp_data[1:], columns=temp_data[0]).rename(columns={'ID': '学生ID', '登録日時': 'Date', '経由点(バナー)': 'ID'})
-<<<<<<< HEAD
 val_register_R = make_register(register_R)
-=======
-register_R = register_R[['Date', 'ID']]
-register_R['pattern'] = register_R['ID'].str[-1]
-temp = register_R.loc[register_R['pattern']=='p']
-temp['ID'] = temp['ID'].str[:-1]
-register_R['ID'].loc[register_R['pattern']=='p'] = temp['ID']
-register_R['Count'] = 1
-register_R['Date'] = register_R['Date'].str.split(' ', expand=True)[0]
-register_R['Date'] = pd.to_datetime(register_R['Date'])
-register_R['Date'] = register_R['Date'].dt.strftime('%Y/%m/%d')
-val_register_R = pd.pivot_table(data = register_R, values = "Count", aggfunc ="count", index = ["Date", "ID"], margins=False, fill_value=0)
-val_register_R = val_register_R.reset_index()
-val_register_R['type'] = 'cv'
-val = pd.concat([val,val_register_R])
->>>>>>> ca8b102ccae85e1bc320bd0950a852ada3323d66
 
 # LINE登録の計算
 worksheet = sh_member.worksheet(SP_SHEET_REGISTER['SP_SHEET_L'])
 temp_data = worksheet.get_all_values()
 register_L = pd.DataFrame(temp_data[2:], columns=temp_data[1]).rename(columns={'ID': '学生ID', '登録(フォロー)日時': 'Date', '流入経路詳細': 'ID'})
-<<<<<<< HEAD
 val_register_L = make_register(register_L)
 
 val_register = pd.concat([val_register_R, val_register_L])
@@ -207,37 +159,3 @@ del val_month['Date']
 val_month = val_month.groupby(['Month', 'ID']).sum()[['Imp', 'CT', 'CV']]
 val_month = val_month.reset_index()
 val_month = calc_per(val_month)
-=======
-register_L = register_L[['Date', 'ID', '学生ID', '卒業年度', '電話番号']].rename(columns={'学生ID': '友だち', '電話番号': 'Count'})
-register_L['pattern'] = register_L['ID'].str[-1]
-temp = register_R.loc[register_L['pattern']=='p']
-temp['ID'] = temp['ID'].str[:-1]
-register_L['ID'].loc[register_L['pattern']=='p'] = temp['ID']
-register_L['Date'] = register_L['Date'].str.split(' ', expand=True)[0]
-register_L['Date'] = pd.to_datetime(register_L['Date'])
-register_L['Date'] = register_L['Date'].dt.strftime('%Y/%m/%d')
-val_register_L = pd.pivot_table(data = register_L, values = "Count", aggfunc ="count", index = ["Date", "ID"], margins=False, fill_value=0)
-val_register_L = val_register_L.reset_index()
-val_register_L['type'] = 'cv'
-val = pd.concat([val,val_register_L])
-
-
-val['Date'] = pd.to_datetime(val['Date'])
-val['Month'] = val['Date'].dt.strftime("%Y-%m")
-val['Date'] = val['Date'].dt.strftime('%Y/%m/%d')
-val_daily = pd.merge(val, master, left_on='ID', right_on='ID', how='left')
-st.write(val_daily)
-
-# val_month = val.
-
-# values_month = pd.DataFrame(whole_data.groupby('Month').sum()['Unique Pageviews']).rename(columns={'Unique Pageviews': 'UU'})
-# temp_values_R = pd.DataFrame(register_R.groupby('Month').count()['ID']).rename(columns={'ID': '登録'})
-# temp_values_L = pd.DataFrame(register_L.groupby('Month').count()['ID']).rename(columns={'ID': 'LINE'})
-# values_month = pd.merge(values_month, temp_values_R, left_index=True, right_index=True, how="left")
-# values_month = pd.merge(values_month, temp_values_L, left_index=True, right_index=True, how="left")
-# values_month = values_month.fillna(0)
-
-# st.write(values_month.T)
-# chart = get_chart(values_month)
-# st.altair_chart(chart, use_container_width=True)
->>>>>>> ca8b102ccae85e1bc320bd0950a852ada3323d66
